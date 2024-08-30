832019eedca05dc9ce5057b5eb37065483ae5b5c
Zabir03 <alzabir565@gmail.com>
2024-08-30 22:53:23 +0530

    new

--- Array/vector/operation/OperVector/Opration.cpp
+++ Array/vector/operation/OperVector/Opration.cpp
@@ -4,5 +4,11 @@ using namespace std;
 int main(){
      vector<int> v;
      cout<<"Size: "<<v.size()<<endl;
-     cout<<"capacity:"<<v.capacity
+     cout<<"capacity:"<<v.capacity()<<endl;
+
+     v.push_back(1);
+     cout<<"size:"<<v.capacity()<<endl;
+     v.push_back(2);
+     cout<<"Size:"<<v.capacity()<<endl;
+
 }
