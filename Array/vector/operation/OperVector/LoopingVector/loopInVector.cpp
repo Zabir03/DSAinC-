f30f8bde81b511fc80eafc1b0d31642db1110f1e
Zabir03 <alzabir565@gmail.com>
2024-08-30 22:51:47 +0530

    new

---
+++ Array/vector/operation/OperVector/LoopingVector/loopInVector.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v;
+    for(int i=0;i<5;i++){
+        int element;
+        cin>>element;
+        v.push_back(element);
+    }
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+
+    v.insert(v.begin()+2,6);
+
+    //for each
+
+    for(int ele:v){
+        cout<<ele<<" ";
+    }
+    cout<<endl;
+
+    //while
+    int idx=0;
+    while(idx<v.size()){
+        cout<<v[idx++]<<" ";
+    }
+
+    
+    return 0;
+
+}
